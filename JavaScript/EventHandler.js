d96442e4b87914bf0111f1bfccc714293d71694e
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2022-11-22 23:03:47 +0530

    Completed JS Course

---
+++ JavaScript/EventHandler.js
@@ -0,0 +1,25 @@
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+// chaning the heading by taking inoput from the user input
+
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    h1.innerText = input.value
+})
---
+++ JavaScript/Iterating_from_Objects.js
@@ -0,0 +1,10 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
