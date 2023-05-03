c9047611526b44877ed686dd54a945aa414ea7ac
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-05-03 20:29:34 +0530

    Week 3 React Advance

---
+++ React advance/implementing_pointer_position_with_render_props/src/App.js
@@ -0,0 +1,59 @@
+
+import './App.css';
+import { useEffect,useState } from 'react';
+
+const MousePosition=({render})=>{
+  const [mousePosition,setMousePosition] = useState({x:0,y:0});
+
+  useEffect(()=>{
+    const handleMousePositionChange=(e)=>{
+      setMousePosition({x:e.clientX,y:e.clientY});
+    }
+  
+
+  window.addEventListener('mousemove',handleMousePositionChange);
+  return()=>{
+    window.addEventListener('mousemove',handleMousePositionChange);
+  };
+},[]);
+return render({mousePosition});
+};
+
+const PanelMouseLogger=()=>{
+  return(
+    <div className='BasicTracker'>
+      <p> Mouse Position: </p>
+      <MousePosition
+      render={({mousePosition})=>(
+        <div className='Row'>
+          <span> x:{mousePosition.x}</span>
+          <span> y:{mousePosition.y}</span>
+        </div>
+      )}
+      />
+    </div>
+  )
+}
+
+const PointMouseLogger=()=>{
+  return(
+    <MousePosition
+    render={({mousePosition})=>(
+      <p>
+        ({mousePosition.x},{mousePosition.y})
+      </p>
+    )}
+    />
+  )
+}
+function App() {
+return(
+  <div className='App'>
+    <header className='Header'>Little Lemon Restaurant 🍕🍕🍕</header>
+    <PanelMouseLogger/>
+    <PointMouseLogger/>
+  </div>
+)
+}
+
+export default App;
--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,3 +1,4 @@
+import { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
@@ -5,8 +6,9 @@ import Intro2 from './components/Intro2';
 import Intro1 from './components/Intro1';
 import Nav from './components/Nav';
 import Promo from './components/Promo';
-
-
+import mkp from './components/managingStatewithinComponent';
+import ReactPlayer from 'react-player/youtube'
+import InputComponent from './components/usingHooks';
 function Heading() {
   return (<h1>This is an h1 heading</h1>)
 }
@@ -16,14 +18,57 @@ function App() {
     <div className="App"> 
       This is the starting code for "Your first component" ungraded lab 
       <Heading /> 
+      <MyVideo/>
       <Nav/>
       <Promo/>
       <Intro2/>
       <Intro1/>
       <Footer/>
+      
+      <mkp/>
+      {/* <InputComponent/> */}
+
+{/* How to add Sound file  */}
+    <button onClick={toggle1}>Caspian Tern1</button>
+    <button onClick={toggle2}>Caspian Tern2</button>
     </div> 
+    
   ); 
 } 
- 
+const MyVideo=()=>{
+  return(
+    <ReactPlayer url='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+    playing={false}
+    volume={0.1}
+    seek
+    />
+  );
+};
+/* How to add Sound file  */
+const bird1=new Audio("https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3");
+const bird2=new Audio("https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3");
+
+function toggle1() {
+  if (bird1.paused)
+  {
+    bird1.play();
+  }else
+  {
+    bird1.pause();
+  }
+};
+
+function toggle2() {
+  if (bird2.paused)
+  {
+    bird2.play();
+  }else
+  {
+    bird2.pause();
+  }
+};
+
+
+
 export default App;
 
